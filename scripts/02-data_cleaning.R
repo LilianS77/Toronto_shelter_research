1395916ed6ad4647ecf0a4cc1389fd6950b498da
LilianS77 <xizisun@gmail.com>
2024-09-27 11:17:19 -0400

    styler scripts for data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by 1 observer.
 # Author: Xizi Sun
-# Date: 23 September 2024 
+# Date: 23 September 2024
 # Contact: xizi.sun@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Run the file "01-download_data.R".
@@ -11,13 +11,14 @@
 # Load necessary library
 library(dplyr)
 
-# Load the raw data 
+# Load the raw data
 raw_data <- read_csv(here::here("data", "raw_data", "raw_data.csv")) %>%
-
-# Perform data cleaning: keep only the columns that were simulated
-cleaned_data <- raw_data %>%
-  select(date.mmm.yy., population_group, returned_from_housing, returned_to_shelter, 
-         ageunder16, age16.24, age25.34, age35.44, age45.54, age55.64, age65over)
+  # Perform data cleaning: keep only the columns that were simulated
+  cleaned_data() <- raw_data %>%
+  select(
+    date.mmm.yy., population_group, returned_from_housing, returned_to_shelter,
+    ageunder16, age16.24, age25.34, age35.44, age45.54, age55.64, age65over
+  )
 
 # Preview the cleaned data
 head(cleaned_data)
